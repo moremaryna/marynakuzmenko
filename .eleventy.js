87407b189782a4c9e3eb0e1d969342c0bf8552a0
moremaryna <mary.seleznova@gmail.com>
2026-04-03 08:16:15 +0100

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -12,13 +12,3 @@ module.exports = function (eleventyConfig) {
     }
   };
 };
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
-  return {
-    dir: {
-      input: "src",
-      output: "_site"
-    }
-  };
-};
